61c87707503a90cac06da46bea4c63ea5a7263ce
Aniket Digge <kccaniketd2016@gmail.com>
2022-01-12 16:26:39 +0530

    All set

--- TechAgro/customer/urls.py
+++ TechAgro/customer/urls.py
@@ -6,10 +6,14 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 urlpatterns = [
     path("", views.index, name="CustomerHomes"),
-    path("login/", views.login, name="CustomerLogin"),
-    path("grains/", views.grains, name="CustomerGrains"),
-    path("vegitables/", views.vegitables, name="CustomerVegitables"),
+    #path("login/", views.login, name="CustomerLogin"),
+    #path("grains/", views.grains, name="CustomerGrains"),
+    #path("vegitables/", views.vegitables, name="CustomerVegitables"),
     path("contactus/", views.contactus, name="ContactUs"),
     path("aboutus/", views.aboutus, name="AboutUs"),
+    path("tracker/", views.tracker, name="Tracker"),
+    path("search/", views.search, name="Search"),
+    path("productView/", views.productView, name="ProductView"),
+    path("checkout/", views.checkout, name="CheckOut"),
 ]
 urlpatterns += staticfiles_urlpatterns()
