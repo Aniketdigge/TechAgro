e8a59583b5f42fc93ede6ec022d5d5c202e2ee98
Aniket Digge <kccaniketd2016@gmail.com>
2022-01-22 18:05:39 +0530

    Change 22/1/22

--- TechAgro/customer/views.py
+++ TechAgro/customer/views.py
@@ -7,17 +7,7 @@ from django.http import HttpResponse
 from .models import Product
 # Create your views here.
 def index(request):
-    products = Product.objects.all()
-    allProds = []
-    catprods = Product.objects.values('category', 'id')
-    cats = {item['category'] for item in catprods}
-    for cat in cats:
-        prod = Product.objects.filter(category = cat)
-        n = len(prod)
-        nSlides = n//4 + ceil((n/4) - (n//4))
-        allProds.append([prod, range(1, nSlides), nSlides])
-    params = {'allProds':allProds}
-    return render(request, "customer/index.html", params)
+    return render(request, "customer/index.html")
 
 
 #def login(request):
